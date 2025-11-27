2e8fb3deebfcd70400067332a5a87609da203dcb
Nadia Viñabal <nadiavinabal@hotmail.com>
2025-11-27 11:40:07 -0300

    feat(core): run TypeORM migrations automatically in production

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { AppDataSource } from 'data-source';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -30,6 +31,23 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  //ejecutar migraciones en produccion
+  if (process.env.NODE_ENV === 'production') {
+    try {
+      console.log('Initializing datasource...');
+      await AppDataSource.initialize();
+
+      console.log('Running migrations...');
+      await AppDataSource.runMigrations();
+
+      console.log('✔ Migrations executed successfully');
+    } catch (error) {
+      console.error('❌ Error running migrations:', error);
+    }
+  } else {
+    console.log('Skipping migrations (not in production)');
+  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
